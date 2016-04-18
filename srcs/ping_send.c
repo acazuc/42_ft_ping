88b5f224ee55c63f2a9ac9aae90bdbfecf409893
Jukino <acazuc@student.42.fr>
2016-04-18 17:02:50 +0200

    Fixed timing

--- srcs/ping_send.c
+++ srcs/ping_send.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/01 13:30:40 by acazuc            #+#    #+#             */
-/*   Updated: 2016/04/02 20:38:37 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/04/18 17:00:53 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,16 +50,16 @@ void ping_send(t_env *env)
 	struct timeval tv;
 	ssize_t sended;
 
-	if (gettimeofday(&tv, NULL))
-	{
-		ft_putendl_fd("Can't gettimeofday", 2);
-		exit(EXIT_FAILURE);
-	}
 	env->count++;
 	if (env->v && !env->has_received && env->count != 1) {
 		printf("%lu bytes from %s: type=%d code=%d\n", sizeof(t_packet) - sizeof(struct iphdr), env->ip, 11, 0);
 	}
 	env->has_received = 0;
+	if (gettimeofday(&tv, NULL))
+	{
+		ft_putendl_fd("Can't gettimeofday", 2);
+		exit(EXIT_FAILURE);
+	}
 	ft_memcpy(packet.data, &tv, sizeof(tv));
 	build_ip_header(env, &packet.ip_header);
 	build_icmp_header(env, &packet.icmp_header);
@@ -68,5 +68,5 @@ void ping_send(t_env *env)
 		ft_putendl_fd("ft_ping: can't send packet", 2);
 		exit(EXIT_FAILURE);
 	}
-	env->sended = epoch_micro();
+	env->last_send = epoch_micro();
 }
