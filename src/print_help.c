dc65ee64f08302f3633c891ba5fda6e9058291b9
Jukino <acazuc@student.42.fr>
2016-10-07 21:54:34 +0200

    Better help

--- src/print_help.c
+++ src/print_help.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/01 09:52:39 by acazuc            #+#    #+#             */
-/*   Updated: 2016/04/01 21:10:11 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/10/07 21:54:21 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void print_help(int fd)
 {
-	ft_putstr_fd("Usage: ft_ping [-hv] destination\n", fd);
+	ft_putstr_fd("ft_ping: send ICMP ECHO to destination\nUsage: ft_ping [-hv] destination\n-h: show help (this page)\n-v: verbose if packets aren't answered\n", fd);
 	if (fd == 2)
 		exit(EXIT_FAILURE);
 	exit(EXIT_SUCCESS);
